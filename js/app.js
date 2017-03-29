f8b42041b531e60b195dc50a949d9370a2519049
luksteroz <lukasz.jeleniewicz@gmail.com>
2017-03-29 09:21:51 +0200

    SoC

--- js/app.js
+++ js/app.js
@@ -65,23 +65,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+//kalkulator- do dopracowania
+
     var dropColor = document.querySelectorAll(".list_arrow");
     var pickColor = document.querySelectorAll(".list_panel");
-    console.log(dropColor, pickColor);
+
     var priceColumn = document.querySelector(".panel_right").children;
-    console.log(priceColumn);
+
     var typeChair = document.querySelector(".panel_left").children;
-    console.log(typeChair);
+
     var checkBox = document.querySelector(".check-box label");
-    console.log(checkBox);
+
     var liElement = document.querySelectorAll(".form li");
     var sum = document.querySelector(".sum strong");
-
-
-
-
-
-
 
 
 
@@ -92,73 +88,76 @@ document.addEventListener("DOMContentLoaded", function () {
         var pickElement = choose.children;
         console.log(pickElement);
 
-for (var i = 0; i < pickElement.length; i++) {
+        for (var i = 0; i < pickElement.length; i++) {
 
-        pickElement[i].addEventListener("click", function (event){
+            pickElement[i].addEventListener("click", function (event){
 
-            var text = this.innerText;
-            typeChair[0].innerText = text;
-            var price = this.dataset.price;
-            priceColumn[0].innerText = Number(price);
-            result = Number(priceColumn[0].innerText);
-            sum.innerText = result;
-            result = 0;
-            choose.classList.toggle("display");
-        });
-    }
+                var text = this.innerText;
+                typeChair[0].innerText = text;
+                var price = this.dataset.price;
+                priceColumn[0].innerText = Number(price);
+                result = Number(priceColumn[0].innerText);
+                sum.innerText = result;
+                choose.classList.toggle("display");
+                result = 0;
+            });
+        }
 
-});
-
-dropColor[1].addEventListener("click", function (event) {
-    event.preventDefault();
-    var choose = this.nextElementSibling;
-    choose.classList.toggle("display");
-    var pickElement = choose.children;
-    console.log(pickElement);
-
-for (var i = 0; i < pickElement.length; i++) {
-
-    pickElement[i].addEventListener("click", function (event){
-
-        var text = this.innerText;
-        typeChair[1].innerText = text;
-        var price = this.dataset.price;
-        priceColumn[1].innerText = Number(price);
-        result = Number(priceColumn[1].innerText);
-        sum.innerText = result + Number(sum.innerText);
-        choose.classList.toggle("display");
     });
-}
 
-});
-
-dropColor[2].addEventListener("click", function (event) {
-    event.preventDefault();
-    var choose = this.nextElementSibling;
-    choose.classList.toggle("display");
-    var pickElement = choose.children;
-    console.log(pickElement);
-
-for (var i = 0; i < pickElement.length; i++) {
-
-    pickElement[i].addEventListener("click", function (event){
-
-        var text = this.innerText;
-        typeChair[2].innerText = text;
-        var price = this.dataset.price;
-        priceColumn[2].innerText = Number(price);
-        result = Number(priceColumn[2].innerText);
-        sum.innerText = result + Number(sum.innerText);
+    dropColor[1].addEventListener("click", function (event) {
+        event.preventDefault();
+        var choose = this.nextElementSibling;
         choose.classList.toggle("display");
-    });
-}
+        var pickElement = choose.children;
+        console.log(pickElement);
 
-});
+        for (var i = 0; i < pickElement.length; i++) {
+
+            pickElement[i].addEventListener("click", function (event){
+
+                var text = this.innerText;
+                typeChair[1].innerText = text;
+                var price = this.dataset.price;
+                priceColumn[1].innerText = Number(price);
+                result = Number(priceColumn[1].innerText);
+                sum.innerText = result + Number(sum.innerText);
+                choose.classList.toggle("display");
+            });
+        }
+    });
+
+    dropColor[2].addEventListener("click", function (event) {
+        event.preventDefault();
+        var choose = this.nextElementSibling;
+        choose.classList.toggle("display");
+        var pickElement = choose.children;
+        console.log(pickElement);
+
+        for (var i = 0; i < pickElement.length; i++) {
+
+            pickElement[i].addEventListener("click", function (event){
+
+                var text = this.innerText;
+                typeChair[2].innerText = text;
+                var price = this.dataset.price;
+                priceColumn[2].innerText = Number(price);
+                result = Number(priceColumn[2].innerText);
+                sum.innerText = result + Number(sum.innerText);
+                choose.classList.toggle("display");
+            });
+        }
+
+    });
+
+
     checkBox.addEventListener("click", function (event){
         console.log(this);
         if (this.previousElementSibling.checked == true) {
             priceColumn[3].innerText = "";
             typeChair[3].innerText = "";
+            result = Number(this.previousElementSibling.dataset.price);
+            sum.innerText = Number(sum.innerText) - result;
         }else {
             result = Number(this.previousElementSibling.dataset.price);
             priceColumn[3].innerText = result;
